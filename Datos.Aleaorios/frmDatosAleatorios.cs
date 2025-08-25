b7a0a4e4eccd740b086266ed1848fbca1d74cd6e
moises sanchez <moisessanchezgonzalez29@gmail.com>
2025-08-25 09:33:10 -0600

    Versiòn_02: Formulario Datos Aleatorios

---
+++ Datos.Aleaorios/frmDatosAleatorios.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Datos.Aleaorios
+{
+    public partial class frmDatosAleatorios : Form
+    {
+        public frmDatosAleatorios()
+        {
+            InitializeComponent();
+        }
+    }
+}
