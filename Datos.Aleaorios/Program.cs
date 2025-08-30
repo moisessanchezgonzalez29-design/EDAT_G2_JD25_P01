0879b8ab78704f0acc685a73d09479c68ca03c83
moises sanchez <moisessanchezgonzalez29@gmail.com>
2025-08-29 19:28:45 -0600

    Datos aleatorios

--- Datos.Aleaorios/Program.cs
+++ Datos.Aleaorios/Program.cs
@@ -1,22 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace Datos.Aleaorios
+﻿e->mes = aleatorio(1, 12);
+e->anio = aleatorio(1950, 2022);
+sexoAleatorio(e->sexo);
+grupoAleatorio(e->grupo);
+e->sueldo = (float)(aleatorio(8000, 20000));
+e->seguroMedico = aleatorio(0, 1);
+}
+void imprimirEmpleado(Empleado e)
 {
-    internal static class Program
-    {
-        /// <summary>
-        /// Punto de entrada principal para la aplicación.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmDatosAleatorios());
-        }
-    }
+    printf("Datos del empleado:\n");
+    printf("Número: %d\n", e.numero);
+    printf("Nombre: %s\n", e.nombre);
+    printf("Fecha nacimiento: %d de %s de %d\n", e.dia, mesAleatorio(e.mes), e.anio);
+    printf("Sexo: %s\n", e.sexo);
+    printf("Grupo: %s\n", e.grupo);
+    printf("Sueldo: %.2f\n", e.sueldo);
+    printf("Seguro médico: %s\n", e.seguroMedico ? "Sí" : "No");
+}
+int main()
+{
+    srand(time(NULL));  // Inicializar generador aleatorio
+    Empleado empleado;
+    generarEmpleadoAleatorio(&empleado);
+    imprimirEmpleado(empleado);
+    return 0;
 }
