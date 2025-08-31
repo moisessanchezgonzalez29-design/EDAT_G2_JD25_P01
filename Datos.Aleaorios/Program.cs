6f3735a62c54c04a0bb3a0fc360015c6be4d181a
moises sanchez <moisessanchezgonzalez29@gmail.com>
2025-08-30 21:09:19 -0600

    Datos Aleatorios

--- Datos.Aleaorios/Program.cs
+++ Datos.Aleaorios/Program.cs
@@ -1,28 +1,17 @@
-﻿e->mes = aleatorio(1, 12);
-e->anio = aleatorio(1950, 2022);
-sexoAleatorio(e->sexo);
-grupoAleatorio(e->grupo);
-e->sueldo = (float)(aleatorio(8000, 20000));
-e->seguroMedico = aleatorio(0, 1);
-}
-void imprimirEmpleado(Empleado e)
+namespace EmpleadoDatosAleatorios
 {
-    printf("Datos del empleado:\n");
-    printf("Número: %d\n", e.numero);
-    printf("Nombre: %s\n", e.nombre);
-
-    printf("Fecha nacimiento: %d de %s de %d\n", e.dia, mesAleatorio(e.mes), e.anio);
-    printf("Sexo: %s\n", e.sexo);
-    printf("Grupo: %s\n", e.grupo);
-    printf("Sueldo: %.2f\n", e.sueldo);
-    printf("Seguro médico: %s\n", e.seguroMedico ? "Sí" : "No");
-
-}
-int main()
-{
-    srand(time(NULL));  // Inicializar generador aleatorio
-    Empleado empleado;
-    generarEmpleadoAleatorio(&empleado);
-    imprimirEmpleado(empleado);
-    return 0;
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
 }
